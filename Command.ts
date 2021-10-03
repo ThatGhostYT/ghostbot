8a1480a955057fcb6f9474d1267e0b7ed9f69162
That Ghost <82480584+ThatGhostYT@users.noreply.github.com>
2021-10-03 16:19:41 +0000

    initial commit

---
+++ Command.ts
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+
+interface CommandOptions{
+	name: string;
+	description: string;
+	callback: (message: Discord.Message, args: string[], client: Discord.Client, Discord: Discord);
+}
+
+module.exports = class Command{
+	constructor(options: CommandOptions){
+		this.name = options.name;
+		this.description = options.description;
+		this.callback = options.callback;
+	}
+}
